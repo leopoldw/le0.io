c01afc0ed5f941f556193968619d8ba17d927b60
Leopold Wicht <leowicht@gmail.com>
2019-02-21 15:37:40 +0000

    attempt5

--- src/functions/requestinfo.js
+++ src/functions/requestinfo.js
@@ -1,23 +1,32 @@
-import geolocationAPI from 'ip-geolocation-api-javascript-sdk'
-import GeolocationParams from 'ip-geolocation-api-javascript-sdk/GeolocationParams.js'
+// import GeolocationAPI from 'ip-geolocation-api-javascript-sdk'
+// import GeolocationParams from 'ip-geolocation-api-javascript-sdk/GeolocationParams.js'
+
+const GeolocationAPI = require(`ip-geolocation-api-javascript-sdk`)
+const GeolocationParams = require(`ip-geolocation-api-javascript-sdk/GeolocationParams.js`)
 
 // https://app.ipgeolocation.io/
-const geolocation = geolocationAPI(process.env.GEOLOCATION_TOKEN, false)
+const geolocation = new GeolocationAPI(process.env.GEOLOCATION_TOKEN, false)
 
 exports.handler = ({ headers }, context, callback) => {
   const ip = headers[`client-ip`] || headers[`x-bb-ip`] || headers[`x-forwarded-for`]
   const language = headers[`x-language`]
 
   if (!ip)
-    callback(`no IP found`)
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        ip,
+        language,
+      }),
+    })
 
   const params = new GeolocationParams()
   params.setIPAddress(ip)
 
   geolocation.getGeolocation((geo) => {
     callback(null, {
-      status: 200,
-      body: { ip, language, geo },
+      statusCode: 200,
+      body: JSON.stringify({ ip, language, geo }),
     })
   }, params)
 }
