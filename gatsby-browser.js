fb476dd1f1d4e7c697c07cf082eea87aea42383e
Leopold Wicht <leowicht@gmail.com>
2019-03-13 17:11:58 +0100

    readd fe tag

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,5 @@ const LogRocket = require(`logrocket`)
 
 exports.onClientEntry = () => {
   if (process.env.NODE_ENV === `production`)
-    LogRocket.init(process.env.LOGROCKET)
+    LogRocket.init(`xpnudi/le0io`)
 }
