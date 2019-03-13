ce4bec6c524ae6618786adc2d34f9bfe1262b0fe
Leopold Wicht <leowicht@gmail.com>
2019-03-13 15:58:10 +0100

    add as env var

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,5 @@ const LogRocket = require(`logrocket`)
 
 exports.onClientEntry = () => {
   if (process.env.NODE_ENV === `production`)
-    LogRocket.init(`xpnudi/le0io`)
+    LogRocket.init(process.env.LOGROCKET)
 }
